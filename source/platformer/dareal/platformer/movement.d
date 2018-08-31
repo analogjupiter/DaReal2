59ca7c15fc0ae3b37d122b5f479e815477f313e4
0xEAB <desisma@heidel.beer>
2018-08-31 04:11:00 +0200

    Implement acceleration/braking helper
    Usage examples:
     - vehicles
     - jumping + falling down

--- source/platformer/dareal/platformer/movement.d
+++ source/platformer/dareal/platformer/movement.d
@@ -19,11 +19,95 @@ public
     Don't pass the jumpthrough matrix to this function!
  +/
 alias collideJumpUp(MatrixWalls) = collide!(ScanProcedure.borderTopOnly, MatrixWalls);
+
+/++
+    Pass all matrices containing blocks the character could land on to this function.
+    If there're obstacles the player could hit pass their matrix, too.
+ +/
 alias collideJumpFalling(MatrixWallsAndJumpThroughBlocks) = collide!(
         ScanProcedure.borderBottomOnly, MatrixWallsAndJumpThroughBlocks);
+
 alias collideWalkLeft(MatrixWallsAndJumpThroughBlocks) = collide!(ScanProcedure.borderLeftOnly);
 alias collideWalkRight(MatrixWallsAndJumpThroughBlocks) = collide!(
         ScanProcedure.borderRightOnly, MatrixWallsAndJumpThroughBlocks);
 
 alias canFallThrough(MatrixFallThroughBlocks) = collide!(
         ScanProcedure.borderBottomOnly, MatrixFallThroughBlocks);
+
+/++
+    Helper for acceleration and braking
+ +/
+struct Speed
+{
+    /++
+        Current speed
+     +/
+    float speedCurrent;
+
+    /++
+        Acceleration
+
+        Speed-up factor
+
+        Possible values:
+            = 1 ... no acceleration, won't speed up
+            > 1 ... accelerate by the given factor
+     +/
+    float acceleration;
+
+    /++
+        Brakeforce
+
+        Slow-down factor
+
+        Possible values:
+            = 1 ... no brakes, won't slow down
+            < 1 ... brake by the given factor
+            = 0 ... instant braking
+     +/
+    float brakeforce;
+
+    /++
+        Minimum speed
+
+        Is not be enforced.
+        A misconfigured brakeforce will allow going below this threshold.
+     +/
+    float speedMin;
+
+    /++
+        Maximum speed
+
+        Is not be enforced.
+        A misconfigured brakeforce will allow going above this threshold.
+     +/
+    float speedMax;
+
+    /++
+        Returns:
+            A copy with acceleration or braking applied
+     +/
+    Speed next(bool accelerate) const
+    {
+        if (accelerate)
+        {
+            if (this.speedCurrent >= this.speedMax)
+            {
+                return this;
+            }
+
+            return Speed((this.speedCurrent * this.acceleration),
+                    this.acceleration, this.speedMin, this.speedMax);
+        }
+
+        // brake
+
+        if (this.speedCurrent <= this.speedMin)
+        {
+            return this;
+        }
+
+        return Speed((this.speedCurrent * this.brakeforce), this.acceleration,
+                this.speedMin, this.speedMax);
+    }
+}
